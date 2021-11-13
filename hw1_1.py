f2638080176c6422ef0d3f5782cacf59cb622ada
Jan <grotius.uy@gmail.com>
2021-11-13 13:32:59 -0300

    hw4 parte1

--- hw1_1.py
+++ hw1_1.py
@@ -1,4 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy
 import numpy as np
+from sklearn.linear_model import LinearRegression, SGDClassifier
 
 
 def ex1():
@@ -8,24 +11,18 @@ def ex1():
     x = np.array([x1, x2, x3])
     y = np.array([1, -1, 1])
 
-def perceptron(x, y, T):
+def perceptron(x, y, T=50):
     theta_0 = 0
     theta = np.zeros(x.shape[1])
-
-#    theta = np.array([-4,2.5])
-#    theta_0 = -2
-
-    print("T = {}".format(T))
-    print("theta = {}; theta0 = {}".format(theta, theta_0))
-#    print("x.shape[0]={}".format(x.shape[0]))
+    conteo = np.zeros(x.shape[0])
     for t in range(T):
         for i in range(x.shape[0]):
             if y[i]*(theta@x[i] + theta_0) <= 0:
                 theta = theta + y[i]*x[i]
                 theta_0 = theta_0 + y[i]
-                print("theta = {}; theta0 = {}; iteracion: i={}, t={}".format(theta, theta_0, i,t))
-    return theta, theta_0
-
+                conteo[i] = conteo[i] + 1
+#    print(conteo)
+    return theta, theta_0, conteo
 
 def perceptron_origin(x, y, T):
     theta = np.zeros(x.shape[1])
@@ -37,6 +34,52 @@ def perceptron_origin(x, y, T):
                 print(theta)
 
     return theta
+
+
+def kernel_function(a, b):
+    return a@b.T + np.square(a@b.T)
+
+def suma_kernels(i, x, y, alfa):
+    resultado = 0
+    for j in range(x.shape[0]):
+        # if numpy.not_equal(j,i):
+        # resultado = resultado + alfa[j]*y[j]*kernel_function(x[j], x[i])
+        k = x[i,0]*x[j,0]+x[i,1]*x[j,1]+2*x[i,0]*x[i,1]*x[j,0]*x[j,1]+x[i,0]^2*x[j,0]^2+x[i,1]^2*x[j,1]^2
+        resultado = resultado + alfa[j]*y[j]*k
+    return resultado
+
+def closed_form(x,y):
+    n = x.shape[0]
+    b = np.zeros(x.shape[1] + 1)
+    A = np.zeros([x.shape[1] + 1, x.shape[1] + 1])
+    for t in range(n):
+        xt = np.append(x[t], [1])
+        b = b + y[t]*xt
+        A = A + xt.reshape([3,1]) @ xt.reshape([3,1]).T
+    b = (1/n) * b
+    A = (1/n) * A
+
+    theta = b @ numpy.linalg.inv(A)
+
+    return theta
+
+
+
+def kernel_perceptron(x, y, T=350):
+    conteo = np.zeros(x.shape[0])
+    alfa = np.zeros(x.shape[0])
+    for t in range(T):
+        for i in range(x.shape[0]):
+            if y[i] * suma_kernels(i, x, y, alfa) <= 0:
+                alfa[i] = alfa[i] + 1
+                conteo[i] = conteo[i] + 1
+    #    print(conteo)
+    theta = 0
+    for i in range(x.shape[0]):
+        fi = np.array([x[0], x[1], x[0]^2, np.sqrt(2)*x[0]*x[1], x[1]^2])
+        theta = theta + alfa[i]*y[i]*fi
+
+    return alfa, conteo, theta
 
 
 def clasificar(theta, theta0, x):
@@ -55,24 +98,67 @@ if __name__ == "__main__":
     y = np.array([y1, y2, y3])
     perceptron_origin(x, y, 10)
     """
-    x1 = np.array([-4,2], dtype=float)
-    x2 = np.array([-2,1], dtype=float)
-    x3 = np.array([-1,-1], dtype=float)
-    x4 = np.array([2,2], dtype=float)
-    x5 = np.array([1,-2], dtype=float)
+    x1 = np.array([0, 0, -1, 1], dtype=float)
+    x2 = np.array([2, 0, -1, 9], dtype=float)
+    x3 = np.array([3, 0, -1, 10], dtype=float)
+    x4 = np.array([0, 2, -1, 5], dtype=float)
+    x5 = np.array([2, 2, -1, 9], dtype=float)
+    x6 = np.array([5, 1, 1, 11], dtype=float)
+    x7 = np.array([5, 2, 1, 0], dtype=float)
+    x8 = np.array([2, 4, 1, 3], dtype=float)
+    x9 = np.array([4, 4, 1, 1], dtype=float)
+    x10 = np.array([5, 5, 1, 1], dtype=float)
 
-    x = np.array([x1, x2, x3, x4, x5])
+    matrix = np.array([x1,x2,x3,x4,x5,x6,x7,x8,x9,x10])
 
-    y1 = 1
-    y2 = 1
-    y3 = -1
-    y4 = -1
-    y5 = -1
+    x = matrix[:,0:2]
+    y = matrix[:,2]
 
-    y = np.array([y1, y2, y3, y4, y5])
+    theta = closed_form(x,y)
+    print("Closed Form: {}".format(theta))
 
-    for t in range(3):
-        theta, theta0 = perceptron(x, y, t)
-#        for item in x:
-#            print(clasificar(theta, theta0, item))
+
+    errores = matrix[:,3]
+    conteo = np.zeros(x.shape[0])
+    '''
+    while not numpy.array_equal(conteo, errores):
+        matrix = numpy.random.permutation(matrix)
+        x = matrix[:, 0:2]
+        y = matrix[:, 2]
+        errores = matrix[:, 3]
+        theta, theta0, conteo = perceptron(x, y)
+
+
+    print("theta = {}; theta0 = {}".format(theta, theta0))
+    print(matrix)
+    
+    reg = LinearRegression().fit(matrix[:, 0:2], matrix[:, 2])
+    print(reg.coef_)
+    print(reg.intercept_)
+'''
+
+    mat2 = np.array([
+        [0,0,-1,1],
+        [2,0,-1,65],
+        [1,1,-1,11],
+        [0,2,-1,31],
+        [3,3,-1,72],
+        [4,1,1,30],
+        [5,2,1,0],
+        [1,4,1,21],
+        [4,4,1,4],
+        [5,5,1,15]
+    ])
+    alfa, conteo, theta = kernel_perceptron(mat2[:,0:2], mat2[:,2])
+    print("Salida kernel perceptron")
+
+    # for veces in range(20):
+    while not numpy.array_equal(conteo, errores):
+        mat2 = numpy.random.permutation(mat2)
+        x = mat2[:, 0:2]
+        y = mat2[:, 2]
+        errores = mat2[:, 3]
+        alfa, conteo, theta = kernel_perceptron(x, y)
+
+    print(theta)
 
--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+import numpy as np
+from sklearn.cluster import KMeans
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def clasificar(x, c0, c1):
+    for element in x:
+        dist0 = np.linalg.norm(element[0:2]-c0, ord=2)
+        dist1 = np.linalg.norm(element[0:2]-c1, ord=2)
+        if dist0 < dist1:
+            element[2] = 0
+        else:
+            element[2] = 1
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def mover(x):
+    c0 = np.array([0,0], dtype=float)
+    c1 = np.array([0,0], dtype=float)
+    sum0 = 0
+    sum1 = 1
+    for element in x:
+        if element[2] == 0:
+            c0 += element[0:2]
+            sum0 += 1
+        else:
+            c1 += element[0:2]
+            sum1 += 1
 
+    return c0/sum0, c1/sum1
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    x = np.array([[0,-6,0], [4,4, 0], [0,0,0], [-5,2,0]], dtype=float)
+    k = 2
+    c0, c1 = np.array([-5, 2]), np.array([0, -6])
+    for i in range(100):
+        clasificar(x, c0, c1)
+        c0, c1 = mover(x)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(c0)
+    print(c1)
+    print(x)
+
+    y = np.array([[0,-6], [4,4], [0,0], [-5,2]], dtype=float)
+    kmedoids = KMeans(n_clusters=2).fit(y)
+    print(kmedoids.cluster_centers_)
+    print(kmedoids.labels_)
